e2555fec2cf2f21fba6441f35cad8650f0a965d7
Nico De Witte <nico.dewitte@outlook.com>
2019-11-13 10:02:04 +0100

    Step 3 - build initial revealed secret for user

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,6 +8,9 @@ namespace Hangman
         // Place here to make bigger scope, we need it in multiple methods
         static string secret = "";
 
+        // STEP 3 - Revealed secret (includes correct guessed letters)
+        static string revealedSecret = "";
+
         // STEP 1 - Welcome text to user
         static void WelcomeText()
         {
@@ -22,10 +25,31 @@ namespace Hangman
             secret = "Hello World";
         }
 
+        // STEP 3 - Build the initial revealed secret string
+        // Hello World => _ _ _ _ _   _ _ _ _ _
+        static void BuildInitialRevealedSecret()
+        {
+            // This will make revealed secret twice as long as original secret
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] != ' ')
+                {
+                    revealedSecret += "_ ";
+                } else
+                {
+                    revealedSecret += "  ";
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             WelcomeText();      // STEP 1
             GenerateSecret();   // STEP 2
+            BuildInitialRevealedSecret();       // STEP 3
+
+            // Print revealed secret
+            Console.WriteLine($"Current progress: {revealedSecret.Trim()}");
         }
     }
 }
