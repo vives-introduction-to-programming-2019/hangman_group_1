24245b8247e5e708a0eac3ba45b17f7db3786c89
Nico De Witte <nico.dewitte@outlook.com>
2019-11-13 09:41:01 +0100

    Add step 2 - generating a secret

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,6 +4,10 @@ namespace Hangman
 {
     class Program
     {
+        // STEP 2 - The secret
+        // Place here to make bigger scope, we need it in multiple methods
+        static string secret = "";
+
         // STEP 1 - Welcome text to user
         static void WelcomeText()
         {
@@ -12,9 +16,16 @@ namespace Hangman
             Console.WriteLine("You have a number of guesses or its the gallows.");
         }
 
+        // STEP 2 - Generate a secret word
+        static void GenerateSecret()
+        {
+            secret = "Hello World";
+        }
+
         static void Main(string[] args)
         {
             WelcomeText();      // STEP 1
+            GenerateSecret();   // STEP 2
         }
     }
 }
